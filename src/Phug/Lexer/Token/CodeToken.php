10f330b845c8d5f2afaf7940057e9e36d50ae345
Torben Köhn <torben.koehn@mercoline.de>
2016-11-29 18:22:27 +0100

    Code token now also uses EscapeTrait and CheckTrait

--- src/Phug/Lexer/Token/CodeToken.php
+++ src/Phug/Lexer/Token/CodeToken.php
@@ -4,10 +4,14 @@ namespace Phug\Lexer\Token;
 
 use Phug\Lexer\AbstractToken;
 use Phug\Util\Partial\BlockTrait;
+use Phug\Util\Partial\CheckTrait;
+use Phug\Util\Partial\EscapeTrait;
 use Phug\Util\Partial\ValueTrait;
 
 class CodeToken extends AbstractToken
 {
     use ValueTrait;
     use BlockTrait;
+    use EscapeTrait;
+    use CheckTrait;
 }
