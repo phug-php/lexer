2e23ea90a041aac627e94f7e32c572078934c9a2
Kyle <kylekatarnls@gmail.com>
2017-01-21 00:56:19 +0100

    SubScanner test

---
+++ tests/Phug/Lexer/Scanner/SubScannerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Phug\Test\Lexer\Scanner;
+
+use Phug\Lexer\Token\AttributeStartToken;
+use Phug\Lexer\Token\AttributeEndToken;
+use Phug\Lexer\Token\ExpansionToken;
+use Phug\Lexer\Token\TagToken;
+use Phug\Lexer\Token\TextToken;
+use Phug\Test\AbstractLexerTest;
+
+class SubScannerTest extends AbstractLexerTest
+{
+    /**
+     * @covers Phug\Lexer\Scanner\SubScanner
+     * @covers Phug\Lexer\Scanner\SubScanner::scan
+     */
+    public function testScan()
+    {
+        $this->assertTokens('p Hello', [
+            TagToken::class,
+            TextToken::class,
+        ]);
+
+        $this->assertTokens('p. Hello', [
+            TagToken::class,
+            TextToken::class,
+        ]);
+
+        $this->assertTokens('p! Hello', [
+            TagToken::class,
+            TextToken::class,
+        ]);
+
+        $this->assertTokens('p!. Hello', [
+            TagToken::class,
+            TextToken::class,
+        ]);
+
+        $this->assertTokens('p:!. Hello', [
+            TagToken::class,
+            ExpansionToken::class,
+            TextToken::class,
+        ]);
+    }
+}
