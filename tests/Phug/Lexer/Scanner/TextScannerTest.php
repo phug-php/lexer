1d8ba2f6b97d5b251494fadd4082292518ac5701
KyleKatarn <kylekatarnls@gmail.com>
2017-01-26 18:15:07 +0100

    Add unit tests

---
+++ tests/Phug/Lexer/Scanner/TextScannerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Phug\Test\Lexer\Scanner;
+
+use Phug\Lexer\Token\TextToken;
+use Phug\Test\AbstractLexerTest;
+
+class TextScannerTest extends AbstractLexerTest
+{
+    /**
+     * @covers Phug\Lexer\Scanner\TextScanner
+     * @covers Phug\Lexer\Scanner\TextScanner::scan
+     */
+    public function testText()
+    {
+        /* @var TextToken $tok */
+        list($tok) = $this->assertTokens('| foo', [
+            TextToken::class,
+        ]);
+
+        self::assertSame('foo', $tok->getValue());
+    }
+}
