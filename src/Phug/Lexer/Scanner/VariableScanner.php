05178f0c6659c3fce750779e0c97a8901945e225
Kyle <kylekatarnls@users.noreply.github.com>
2017-03-07 12:44:01 +0100

    Use official PHP doc variable name format
    Allow such variables like $§foo°bar that are valid in PHP

--- src/Phug/Lexer/Scanner/VariableScanner.php
+++ src/Phug/Lexer/Scanner/VariableScanner.php
@@ -12,7 +12,7 @@ class VariableScanner implements ScannerInterface
     {
         return $state->scanToken(
             VariableToken::class,
-            '\$(?<name>[a-zA-Z_][a-zA-Z0-9_]*)'
+            '\$(?<name>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]**)'
         );
     }
 }
