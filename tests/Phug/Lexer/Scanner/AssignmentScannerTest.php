8749c7e36b149069902bdc049fe71aa86f7bf9c7
KyleKatarn <kylekatarnls@gmail.com>
2018-02-06 16:00:37 +0100

    Allow to nest complex expressions in ternary https://github.com/phug-php/phug/issues/22

--- tests/Phug/Lexer/Scanner/AssignmentScannerTest.php
+++ tests/Phug/Lexer/Scanner/AssignmentScannerTest.php
@@ -6,6 +6,7 @@ use Phug\Lexer\Token\AssignmentToken;
 use Phug\Lexer\Token\AttributeEndToken;
 use Phug\Lexer\Token\AttributeStartToken;
 use Phug\Lexer\Token\AttributeToken;
+use Phug\Lexer\Token\TagToken;
 use Phug\Test\AbstractLexerTest;
 
 class AssignmentScannerTest extends AbstractLexerTest
@@ -16,7 +17,6 @@ class AssignmentScannerTest extends AbstractLexerTest
      */
     public function testScan()
     {
-
         /** @var AssignmentToken $tok */
         list($tok) = $this->assertTokens('&test', [
             AssignmentToken::class,
@@ -27,7 +27,6 @@ class AssignmentScannerTest extends AbstractLexerTest
 
     public function testScanWithAttributes()
     {
-
         /** @var AssignmentToken $tok */
         list($tok) = $this->assertTokens('&test(a=a b=b c=c)', [
             AssignmentToken::class,
@@ -39,5 +38,28 @@ class AssignmentScannerTest extends AbstractLexerTest
         ]);
 
         self::assertSame('test', $tok->getName());
+    }
+
+    /**
+     * @see https://github.com/phug-php/phug/issues/22
+     */
+    public function testObjectInTernary()
+    {
+        $code = "a()&attributes(isNestedFile ? {'href': '../account-orders.html'} : {'href': 'accountorders.html'})";
+        /** @var AttributeToken $tok */
+        list(, , , , , $tok) = $this->assertTokens($code, [
+            TagToken::class,
+            AttributeStartToken::class,
+            AttributeEndToken::class,
+            AssignmentToken::class,
+            AttributeStartToken::class,
+            AttributeToken::class,
+            AttributeEndToken::class,
+        ]);
+
+        self::assertSame(
+            'isNestedFile ? {\'href\': \'../account-orders.html\'} : {\'href\': \'accountorders.html\'}',
+            $tok->getName()
+        );
     }
 }
