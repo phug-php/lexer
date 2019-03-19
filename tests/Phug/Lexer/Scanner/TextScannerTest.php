7c76d1db56fd892a50ad22145bafefdd1301bbba
KyleKatarn <kylekatarnls@gmail.com>
2019-03-19 15:33:54 +0100

    Simplify the line analyzer

--- tests/Phug/Lexer/Scanner/TextScannerTest.php
+++ tests/Phug/Lexer/Scanner/TextScannerTest.php
@@ -3,6 +3,7 @@
 namespace Phug\Test\Lexer\Scanner;
 
 use Phug\Lexer\Token\CommentToken;
+use Phug\Lexer\Token\FilterToken;
 use Phug\Lexer\Token\IndentToken;
 use Phug\Lexer\Token\NewLineToken;
 use Phug\Lexer\Token\TagToken;
@@ -209,5 +210,111 @@ class TextScannerTest extends AbstractLexerTest
         $text = end($tokens);
 
         self::assertSame("\n  foo\n x\n  bar\n  baz", $text->getValue());
+    }
+
+    /**
+     * @covers \Phug\Lexer\Analyzer\LineAnalyzer::<public>
+     */
+    public function testWhitespaceWithFilter()
+    {
+        $template = implode("\n", [
+            'div',
+            '  :pre',
+            '    foo',
+            ' ',
+            '    bar',
+            '    baz',
+        ]);
+
+        $tokens = $this->assertTokens($template, [
+            TagToken::class,
+            NewLineToken::class,
+            IndentToken::class,
+            FilterToken::class,
+            NewLineToken::class,
+            IndentToken::class,
+            TextToken::class,
+        ]);
+
+        /** @var TextToken $text */
+        $text = end($tokens);
+
+        self::assertSame("\nfoo\n\nbar\nbaz", $text->getValue());
+
+        $template = implode("\n", [
+            'div',
+            '  :pre',
+            '    foo',
+            '   ',
+            '    bar',
+            '    baz',
+        ]);
+
+        $tokens = $this->assertTokens($template, [
+            TagToken::class,
+            NewLineToken::class,
+            IndentToken::class,
+            FilterToken::class,
+            NewLineToken::class,
+            IndentToken::class,
+            TextToken::class,
+        ]);
+
+        /** @var TextToken $text */
+        $text = end($tokens);
+
+        self::assertSame("\nfoo\n\nbar\nbaz", $text->getValue());
+
+        $template = implode("\n", [
+            'div',
+            '  :pre',
+            '    foo',
+            '   x',
+            '    bar',
+            '    baz',
+        ]);
+
+        $tokens = $this->assertTokens($template, [
+            TagToken::class,
+            NewLineToken::class,
+            IndentToken::class,
+            FilterToken::class,
+            NewLineToken::class,
+            IndentToken::class,
+            TextToken::class,
+        ]);
+
+        /** @var TextToken $text */
+        $text = end($tokens);
+
+        self::assertSame("\n foo\nx\n bar\n baz", $text->getValue());
+    }
+
+    /**
+     * @covers \Phug\Lexer\Analyzer\LineAnalyzer::<public>
+     */
+    public function testWhitespaceWithMarkup()
+    {
+        $template = implode("\n", [
+            'div',
+            '  <pre>',
+            '    foo',
+            ' ',
+            '    bar',
+            '    baz',
+            '  </pre>',
+        ]);
+
+        $tokens = $this->assertTokens($template, [
+            TagToken::class,
+            NewLineToken::class,
+            IndentToken::class,
+            TextToken::class,
+        ]);
+
+        /** @var TextToken $text */
+        $text = end($tokens);
+
+        self::assertSame("<pre>\n  foo\n\n  bar\n  baz\n</pre>", $text->getValue());
     }
 }
